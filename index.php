83b28e916de0dfc092ab16af330b56b899004d09
gwindblaids <gwindblaids@gmail.com>
2018-04-05 00:47:58 +0300

    Add custom style for autocomplete element JQuery

--- index.php
+++ index.php
@@ -13,7 +13,6 @@ header("Content-Type = text/html;charset=utf-8;");?>
         <meta charset="utf-8">
         <link type="text/css" rel="stylesheet" href="css/style.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
         <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
         <script type="text/javascript" src="js/script.js"></script>
         <link rel="shortcut icon" href="images/course_work.jpg" type="image/jpg">
