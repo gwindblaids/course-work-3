d5a8043ed3e851475191ffad23249ba6cd42d476
gwindblaids <gwindblaids@gmail.com>
2018-04-13 00:35:14 +0300

    add destructor for class Database. Refactoring code in files query.php and search.php

--- includes/database.php
+++ includes/database.php
@@ -4,7 +4,7 @@
  * @Author: gwindblaids
  * @Date:   2018-03-29 16:25:07
  * @Last Modified by:   gwindblaids
- * @Last Modified time: 2018-04-09 21:01:52
+ * @Last Modified time: 2018-04-12 12:25:54
  */
 	class Database {
 		public $db = '';
@@ -23,6 +23,9 @@
 			}
 		}
 
+		function __destruct() {
+			$this->db=null;
+		}
 	}
 	$database = new Database;
  ?>
