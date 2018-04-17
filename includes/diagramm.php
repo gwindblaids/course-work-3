2b686b4bdf243fa93346ecb6752e3545db09282c
gwindblaids <gwindblaids@gmail.com>
2018-04-18 01:15:56 +0300

    Fix conditions for query.php

--- includes/diagramm.php
+++ includes/diagramm.php
@@ -1,12 +1,5 @@
 <?php
-if ($_POST['change']=='cd' && !empty($_POST['query'])) {
-require 'database.php';
-$query = $database->setRequest("SELECT * FROM `users`");
-while ($row = $query->fetch(PDO::FETCH_LAZY)) {
- 	foreach ($row as $key => $value) {
- 		
-   	}
-}
-
+if (isset($_GET['value'])) {
+	echo "yes";
 }
 ?>
--- includes/query.php
+++ includes/query.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @Author: gwindblaids
  * @Date:   2018-03-29 18:17:48
@@ -8,17 +7,12 @@
  */
 if (isset($_POST['query']) && !empty($_POST['query']) && isset($_POST['select_table'])) {
 	echo "<br>";
-			//fr
 			if ($_POST['select_table']=='fr') {
-
 				$query = $database->setRequest("SELECT COUNT(*) AS `COUNT`, cast(`datetime_begin` AS time) AS `begin`, cast(`datetime_end` AS time) AS `end` FROM `users` WHERE cast(`datetime_end` AS date)='" .$_POST['query'] . "' OR cast(`datetime_begin` AS date)='".$_POST['query'] ."' GROUP BY `datetime_begin`, `datetime_end` HAVING COUNT(*) > 1 ORDER BY `COUNT` LIMIT 1");
 				if ($query->rowCount()>0) {
 					echo "<span class=\"description_query\">Максимальное кол-во клиентов на дату " . $_POST['query'] . "</span>";
-
-?>
-<table border="1px" class = "table_dark" align="center">
-<?php
-				echo "<th>Кол-во клиентов</th><th>Время начала сеанса</th><th>Время конца сеанса</th>";
+				echo '<table border="1px" class = "table_dark" align="center">';
+				echo "<tr><th>Кол-во клиентов</th><th>Время начала сеанса</th><th>Время конца сеанса</th></tr>";
 				while ($row = $query->fetch(PDO::FETCH_LAZY)) {
 					
 					echo "<tr>";
@@ -28,20 +22,16 @@ if (isset($_POST['query']) && !empty($_POST['query']) && isset($_POST['select_ta
 					   	}
 					echo "</tr>";
 					}
+					echo '</table>';
 				} else echo "<span class=\"description_query\">К сожалению по вашему запросу  максимальное кол-во клиентов 1.</span>";
 			}
-
 			if ($_POST['select_table']=='sr') {
-				echo "<span class=\"description_query\">Данные операторов с номером смены " . $_POST['query'] . "</span>";
 				$query = $database->setRequest("SELECT full_name_operator, adress, phone FROM receipt WHERE number_shifts=" . $_POST['query']);
-				
-?>
-<table border="1px" class = "table_dark" align="center">
-<?php
-				
-				echo "<th>ФИО</th> <th>Адрес</th><th>Телефон</th>";
+				if ($query->rowCount()>0) {
+				echo "<span class=\"description_query\">Данные операторов с номером смены " . $_POST['query'] . "</span>";
+				echo '<table border="1px" class = "table_dark" align="center">';
+				echo "<tr><th>ФИО</th> <th>Адрес</th><th>Телефон</th></tr>";
 				while ($row = $query->fetch(PDO::FETCH_LAZY)) {
-					
 					echo "<tr>";
 					   	foreach ($row as $key => $value) {
 					   		if ($key!='queryString')
@@ -49,16 +39,14 @@ if (isset($_POST['query']) && !empty($_POST['query']) && isset($_POST['select_ta
 					   	}
 					echo "</tr>";
 					}
-
+					echo '</table>';
+				} else echo "<span class=\"description_query\">К сожалению по вашему запросу ничего не найдено.</span>";
 			}
-
-
-
-			
 			if ($_POST['select_table']=='cd') {
 				echo "<br>Create diagramm query";
 				//$query = $database->setRequest("");
+				//if ($query->rowCount()>0) {
+				//}
 			}
 }
 ?>
-</table>
